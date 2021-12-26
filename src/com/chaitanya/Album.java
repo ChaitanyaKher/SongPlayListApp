4b584ca84c2d1a8944a52d761dd938ecbde1124b
Kher <ckkher@147dda3bad26.ant.amazon.com>
2021-12-26 18:59:25 +0530

    Added new functionalities to the album class
    Album class now finds a song for you in the existing playlist to prevent
    duplicates. You can now add songs to playlist by accesing addToPlayList boolean methods.
    You can call these methods if you have either the song's title or the
    track number.

--- src/com/chaitanya/Album.java
+++ src/com/chaitanya/Album.java
@@ -1,18 +1,58 @@
 package com.chaitanya;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
-    private String name;
+    private String title;
     private String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
-        this.name = name;
+    public Album(String title, String artist, ArrayList<Song> songs) {
+        this.title = title;
         this.artist = artist;
         this.songs = new ArrayList<Song>();
     }
     public Album(){
-        
+
+    }
+
+    public Song findSong(String title){
+        for (Song checkedSong:songs){
+            if (checkedSong.getTitle().equals(title)) return checkedSong;
+        }
+        return null;
+    }
+
+    public boolean addSong(String title, double duration){
+        if(findSong(title)==null){
+            songs.add(new Song(title,duration));
+            System.out.println(title+" successfully added to the list");
+            return true;
+        } else {
+            System.out.println("Song with name "+title+" already exists in the list.");
+            return false;
+        }
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> PlayList){
+        int index = trackNumber -1;
+        if (index>0 && index <= this.songs.size()){
+            PlayList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not song with track number "+trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> PlayList){
+        for (Song checkedSong: this.songs){
+            if (checkedSong.getTitle().equals(title)){
+                PlayList.add(checkedSong);
+                return true;
+            }
+        }
+        System.out.println(title+ " there is no such song in the album");
+        return false;
     }
 }
